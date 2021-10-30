778ad9a30721255482558c7679a30cb124cf539b
thanapato <72536702+thanapato@users.noreply.github.com>
2021-10-31 01:34:11 +0700

    AddMyWork

---
+++ RJARN/Exercise/script.js
@@ -0,0 +1,15 @@
+const word = document.querySelector("#Section1 .group3");
+word.innerHTML = "<b>Ant</b>";
+
+document.querySelector(".group1").textContent = "Cat";
+
+document.getElementsByClassName("group2").innerHTML = "Gu rhao";
+
+let group9 = document.getElementsByClassName("group9")[0];
+group9.append("YOU NEVER CODING ALONE");
+
+const li = document.querySelector("#Section1 .group7");
+li.textContent = "WORK HARD DIE FAST";
+
+let text = document.querySelector("div ul li.group6");
+text.textContent = "Hew Kaw";
